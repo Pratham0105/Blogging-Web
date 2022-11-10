d9743b7cbd097dbe5f021de5e03c3ef1d66600f1
Pratham Parashar <pratham.parashar143858@gmail.com>
2022-11-10 23:27:21 +0530

    First Blogging Templet Using HTML5, CSS3, JavaScript

---
+++ static/main.js
@@ -0,0 +1,23 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick= () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+let searchIcon = document.querySelector('#search-icon');
+let searchForm = document.querySelector('.search-form');
+
+searchIcon.onclick= () =>{
+    searchIcon.classList.toggle('fa-times');
+    searchForm.classList.toggle('active');
+}
+
+window.onscroll= () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    searchIcon.classList.remove('fa-times');
+    searchForm.classList.remove('active');
+}
+
